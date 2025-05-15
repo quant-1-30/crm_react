02486a1075312f321608662ad4bae23264b40635
hengxin.liu <hengxin.liu@inceptio.ai>
2025-05-15 08:08:48 +0800

    revise conf and js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,9 +3,16 @@ import axios from 'axios';
 // 创建 axios 实例
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL || '/api',  // 你的后端 API 地址
+
   timeout: 5000,
+
   //withCredentials: true  // 允许跨域请求携带凭证
-  withCredentials: JSON.parse(process.env.REACT_APP_WITH_CREDENTIALS || 'false')  // 允许跨域请求携带凭证
+  withCredentials: JSON.parse(process.env.REACT_APP_WITH_CREDENTIALS || 'false'), // 允许跨域请求携带凭证
+  
+  // 添加请求头
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });
 
 // 请求拦截器
@@ -30,11 +37,30 @@ instance.interceptors.response.use(
   },
   (error) => {
     if (error.response) {
-      // 处理 401 未授权错误
-      if (error.response.status === 401) {
-        localStorage.removeItem('token');
-        window.location.href = '/login';
+      switch (error.response.status) {
+        case 401:
+          localStorage.removeItem('token');
+          window.location.href = '/login';
+          break;
+        case 403:
+          // 403 无权限
+          console.error('403 无权限');
+          window.location.href = '/403';
+          break;
+        case 404:
+          // 404 未找到
+          console.error('404 未找到');
+          window.location.href = '/404';
+          break;
+        default:
+          console.error('请求错误');
       }
+    } else if (error.request) {
+      // 请求没有响应
+      console.error('请求没有响应');
+    } else {
+      // 其他错误
+      console.error('其他错误, 配置错误');
     }
     return Promise.reject(error);
   }
